b56d3ab8709fd398075dc069097e23f406f82921
Kevin-Cool <kevincoolXD@gmail.com>
2020-06-11 14:47:37 +0200

    classes en Context maken

---
+++ VIP_Services_Rudy_2020/BusinessLayer/Classes/Klant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VIP_Services_Rudy_2020.BusinessLayer.Classes
+{
+    public class Klant
+    {
+        #region Attributes
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public string Categorie { get; set; }
+        public string BTWnummer { get; set; }
+        public string Adres { get; set; }
+        #endregion
+        #region Constructors
+        public Klant(string mNaam,string mCategorie, string mBTWnummer, string mAdres)
+        {
+            SetNaam(mNaam);
+            SetCategorie(mCategorie);
+            SetBTWnummer(mBTWnummer);
+            SetAdres(mAdres);
+        }
+        public Klant()
+        {
+
+        }
+        #endregion
+        #region Setters
+        public void SetNaam(string mNaam)
+        {
+            Naam = mNaam;
+        }
+        public void SetCategorie(string mCategorie)
+        {
+            Categorie = mCategorie;
+        }
+        public void SetBTWnummer(string mBTWnummer)
+        {
+            BTWnummer = mBTWnummer;
+        }
+        public void SetAdres(string mAdres)
+        {
+            Adres = mAdres;
+        }
+        #endregion
+    }
+}
---
+++ VIP_Services_Rudy_2020/PresentationLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VIP_Services_Rudy_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
